6e633a7cf58d22287e59766e799141c6efb0bb79
nferreirax <nferreirax@users.noreply.github.com>
2014-10-18 12:35:01 +0100

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace vendor\nferreirax\banners;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'vendor\nferreirax\banners\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
